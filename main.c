69ef31c3ae39296f0bf655620b8990f30a54a65b
Chicj <chic.jordan@yahoo.com>
2017-02-28 15:03:06 -0900

    created new sol so CW links tasking libs

--- main.c
+++ main.c
@@ -1,94 +1,83 @@
-#include <msp430.h>
-#include <i2c.h>
-#include "bno055.h"
-
-// function prototypes 
-extern int IncrementVcore(void);
-extern int DecrementVcore (void);
-
-
-void main(void)
-{
-//***************************************************************Clock set up*****************************
-// set PM core to acceptable value for 16 MHz pg16 data sheet
-  DecrementVcore();
-  DecrementVcore();
-  DecrementVcore();
-
-  IncrementVcore();
-  IncrementVcore();  // ensure operation in PMM lvl 2 in in order to run 16 MHz clk
-
-// set SMCLK to 16 MHz (default SELREF for FLLREFCLK is XT1CLK = 32*1024 = 32768 Hz = 32.768 KHz)
-  UCSCTL2 = 488;                  // Setting the freq multiplication factor * 1024 for final clk freq @ 16 MHz
-  UCSCTL1 |= DCORSEL_4;         // This sets the DCO frequency pg26 data sheet 12.3 -- 28.3 MHz
-  UCSCTL4 |= SELS__DCOCLKDIV ;  // set output of FLLREFCLK --> DCOCLKDIV to input of SMCLK  <-- This is default 
-//*************************************************************** pin set up ***************************
-  // setup breakout board MCLK and SMCLK test pins for scope testing
-  P11DIR  |= BIT2;  // Set Port 11 pin 2 direction as an output
-  P11SEL |= BIT2;   // Set port 11 pin 2 as SMCLK signal
-
-  //setting up pins for SW1 --> P2.6
-  P2REN |= BIT6 + BIT7;  // turn on internal resistor to combat bounce (this pulls down)
-  P2OUT |= BIT6 + BIT7;  // Pull up when P2REN is high for both switches ( this pulls up), pg 410 fam
-  P2IE |= BIT6 + BIT7;   // Enable IR on P2.6 and P2.7
-
-  P1OUT |= BIT0 + BIT1;  // setup test LED's
-  P1DIR |= BIT0 + BIT1;  // Set LED High on start up   
-
-//************************************************************* I2C set up [UCB1] ****************************
-
-  _EINT();  // set global IR enable 
-  LPM0;
-}
-
- //button IR code
-void Button_IR(void) __interrupt[PORT2_VECTOR]{
-int i=0 ,k=0;
-        switch(P2IV)
-        {
-          case P2IV_NONE: 
-          break; 
-          case P2IV_P2IFG0:
-          break; 
-          case P2IV_P2IFG1: 
-          break; 
-          case P2IV_P2IFG2: 
-          break; 
-          case P2IV_P2IFG3: 
-          break; 
-          case P2IV_P2IFG4:
-          break; 
-          case P2IV_P2IFG5: 
-          break; 
-          case P2IV_P2IFG6: // check button flag, SW1 on breakout board
-             P1DIR ^= BIT0;  // toggle LED when button push
-             
-          break; 
-          case P2IV_P2IFG7:// check button , SW2 on breakout board 
-          break; 
-        }
-}
-
-
-//I2C data ISR, called to transmit or receive I2C data
-void I2C_dat(void) __interrupt[USCI_B1_VECTOR]{
-  switch(UCB1IV){
-    case USCI_I2C_UCNACKIFG:
-      //Not-Acknowledge received
-      UCB1STAT&=~UCNACKIFG;     //clear interrupt flag
-      UCB1CTLW0|=UCTXSTP;       //generate stop condition
-    case USCI_I2C_UCRXIFG:
-      //receive data
-    break;
-    case USCI_I2C_UCTXIFG:
-      //check if there are more bytes
-    break;
-  }  
-} 
-
-//**************** TODO ******************************
-// start a lib for this project to allow more concise code 
-// - timer .h and .c to set SMCLK to 16 MHz as a main start up function.
-//  - make an I2C lib ? 
+//*****************************************************************************************
+//  Using ARC_lib <-- uses port 1
 //
-//*****************************************************
+//
+//*****************************************************************************************
+#include <__cross_studio_io.h>    // whats the diff btwn ctl.h
+#include <msp430.h>
+#include <ctl.h>
+#include <i2c.h>              
+#include <ARCbus.h>
+#include <Error.h>
+#include <terminal.h>         
+#include <UCA2_uart.h>        // UART setup 
+#include "pins.h"             // linked in bare_bones solution 
+#include "subsystem.h"        // linked in bare_bones solution 
+
+
+CTL_TASK_t terminal_task,sub_task,sys_task; // name your task (first thing to do when setting up a new task (1))
+
+//********************************************* allocate mem for tasks (2)
+//stack for terminal
+unsigned terminal_stack[2000];
+//stack for subsystem events
+unsigned sys_stack[1000];
+//stack for bus events
+unsigned sub_stack[1000];
+
+//******************************************** redefine putchar and getchar 
+//make printf and friends use async
+int __putchar(int c){
+  return UCA2_TxChar(c);
+}
+
+//make printf and friends use async
+int __getchar(void){
+  return UCA2_Getc();
+}
+
+//******************************************* Example_Bare_bones main loop
+void main(void){
+  //DO this first
+  ARC_setup();
+
+  //turn on LED's this will flash the LED's during startup
+  P7DIR=0xFF;
+  //init complete turn on LED0 and all others off
+  P7OUT=0x00;
+
+  //TESTING: set log level to report everything by default
+  set_error_level(0);
+
+  //initialize UART
+  UCA2_init_UART(UART_PORT,UART_TX_PIN_NUM,UART_RX_PIN_NUM);
+
+  //BUS_register_cmd_callback(&IMG_parse);
+
+  //setup bus interface
+  initARCbus(0x14);   // Default addr for "SYS" subsystem, should be changed for specific subsystems.
+
+  // initialize stacks (3) 
+  memset(terminal_stack,0xcd,sizeof(terminal_stack));                                           //write known values into the stack 
+  terminal_stack[0]=terminal_stack[sizeof(terminal_stack)/sizeof(terminal_stack[0])-1]=0xfeed;  //put marker values at the words before/after the stack
+  memset(sys_stack,0xcd,sizeof(sys_stack));                                                     //write known values into the stack 
+  sys_stack[0]=sys_stack[sizeof(sys_stack)/sizeof(sys_stack[0])-1]=0xfeed;                      //put marker values at the words before/after the stack
+  memset(sub_stack,0xcd,sizeof(sub_stack));                                                     //write known values into the stack 
+  sub_stack[0]=sub_stack[sizeof(sub_stack)/sizeof(sub_stack[0])-1]=0xfeed;                      //put marker values at the words before/after the stack
+
+
+  // creating the tasks
+  ctl_task_run(&terminal_task,BUS_PRI_LOW,terminal,"Example Bare Bones program ready","terminal",sizeof(terminal_stack)/sizeof(terminal_stack[0])-2,terminal_stack-1,0);
+  ctl_task_run(&sys_task,BUS_PRI_NORMAL,sys_events,NULL,"SYS_events",sizeof(sys_stack)/sizeof(sys_stack[0])-2,sys_stack-1,0);
+  ctl_task_run(&sub_task,BUS_PRI_HIGH,sub_events,NULL,"SUB_events",sizeof(sub_stack)/sizeof(sub_stack[0])-2,sub_stack-1,0);
+
+  //main loop <-- this is an ARCbus function 
+  mainLoop(); 
+
+}
+
+//decode errors
+char *err_decode(char buf[150], unsigned short source,int err, unsigned short argument){
+  sprintf(buf,"source = %i, error = %i, argument = %i",source,err,argument);
+  return buf;
+}
